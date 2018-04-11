caffc56c4c921e1a04afd121e92fdb766dfe9da0
wtm902010 <38243378+wtm902010@users.noreply.github.com>
2018-04-11 12:44:25 +0800

    Initial commit

---
+++ 20180411 Test for Backend.py
@@ -0,0 +1,14 @@
+import re
+urls = [
+    "http://www.google.com/a.txt",
+    "http://www.google.com.tw/a.txt",
+    "http://www.google.com/download/c.jpg",
+    "http://www.google.co.jp/a.txt",
+    "http://www.google.com/b.txt",
+    "https://facebook.com/movie/b.txt",
+    "http://yahoo.com/123/000/c.jpg",
+    "http://gliacloud.com/haha.png"
+    ]
+a = 1
+for i in range(re.search("a.txt", urls)):
+    print("a.txt", a+1)
